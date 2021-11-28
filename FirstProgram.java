2edce709b91e7541b7da01f09be8c24c52aada6e
Sanchet Kumar Sahu <sanchetks98@gmail.com>
2021-11-28 12:02:18 +0530

    Added java First

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FirstProgram 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello world");
+		System.out.print("Welcome to Java: ");
+		
+	}
+
+}
